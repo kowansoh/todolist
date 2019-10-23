a7a571e558b6f575e7e924c89f03f3aed2de119e
unknown <issuser@ISSPIOCRTB5.stf.nus.edu.sg>
2019-10-23 15:58:52 +0800

    Remove unused imports

--- test/ControllerTest.java
+++ test/ControllerTest.java
@@ -1,6 +1,3 @@
-import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
